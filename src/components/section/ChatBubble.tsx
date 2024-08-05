cda775eff798b543e819b9f0680fe5ebd47f68ee
Sumant Kumar Pandey <96868383+sumantP2002@users.noreply.github.com>
2024-08-05 12:25:41 +0530

    done some changes to bot

--- src/components/section/ChatBubble.tsx
+++ src/components/section/ChatBubble.tsx
@@ -3,26 +3,45 @@ import "./ChatBubble.css";
 
 const ChatBubble: React.FC = () => {
   useEffect(() => {
+    // Check if the chat has already been initialized
+    if ((window as any).chatInitialized) return;
+
+    // Mark the chat as initialized
+    (window as any).chatInitialized = true;
+
     // Append the Voiceflow script to the document
     const script = document.createElement("script");
     script.type = "text/javascript";
     script.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
     script.onload = () => {
-      (window as any).voiceflow.chat.load({
-        verify: { projectID: "669247f1ef5a6818a4ea085d" },
-        url: "https://general-runtime.voiceflow.com/",
-        versionID: "production",
-      });
+      (window as any).voiceflow.chat
+        .load({
+          verify: { projectID: "669247f1ef5a6818a4ea085d" },
+          url: "https://general-runtime.voiceflow.com/",
+          versionID: "production",
+        })
+        .then(() => {
+          setTimeout(() => {
+            if ((window as any).voiceflow && (window as any).voiceflow.chat) {
+              (window as any).voiceflow.chat.open(); // Show the chat
+            }
+          }, 0); // Adjust the delay as needed
+        });
     };
+
     const s = document.getElementsByTagName("script")[0];
+    let scriptAppended = false;
     if (s && s.parentNode) {
       s.parentNode.insertBefore(script, s);
+      scriptAppended = true;
     }
 
     // Cleanup script on component unmount
-    // return () => {
-    //   document.body.removeChild(script);
-    // };
+    return () => {
+      if (scriptAppended && script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
   }, []);
 
   return (
